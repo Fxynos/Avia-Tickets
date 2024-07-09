21f52d43ac5349feb1b8aab0ac34797b29e0f10d
Exynos <sinerechka0000@gmail.com>
2024-07-10 00:55:19 +0700

    tickets layout

--- android-client/app/src/main/java/com/vl/aviatickets/ui/screen/AviaTicketsTicketsFragment.kt
+++ android-client/app/src/main/java/com/vl/aviatickets/ui/screen/AviaTicketsTicketsFragment.kt
@@ -9,7 +9,7 @@ import com.vl.aviatickets.databinding.FragmentStubBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class AviaTicketsAllTicketsFragment: Fragment() {
+class AviaTicketsTicketsFragment: Fragment() {
 
     private lateinit var binding: FragmentStubBinding
 
